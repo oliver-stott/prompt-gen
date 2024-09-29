5de8621000765d64bf768a7cec0ba57b8a9065bd
Oliver Stott <ors@oliver-stott.co.uk>
2024-09-29 14:49:33 +0100

    setting up the page layout, removing pages folder, tweaking js config

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <section className="wp-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI Powered Prompts</span>
+      </h1>
+
+      <p className="desc text-center">
+        PromptGen is an open-source AI prompting tool created to help people find and share their creative prompts for the use with AI tools.
+      </p>
+
+      {/* Feed COmponent will go here */}
+
+    </section>
+  )
+}
+
+export default Home
